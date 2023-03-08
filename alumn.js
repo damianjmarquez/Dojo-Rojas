2044707f6783e8ec3f43a58d4a99ced945f1971b
damianjmarquez <69488200+damianjmarquez@users.noreply.github.com>
2023-03-08 05:17:56 -0300

    inicio

--- alumn.js
+++ alumn.js
@@ -1,12 +1,13 @@
 (async () =>{
 const {value: Nombre}= await swal.fire({
-    title: "Hola bienvenido a la falimia ",
+    title: "Hola bienvenido a la familia ",
     html: "<p>INGRESA TU NOMBRE</p>",
     input: "text",
     color:"white",
     inputPlaceholder: "Nombre",
     confirmButtonText:"Adelante",
-    background:"rgb(36, 36, 36)"
+    background:"rgb(36, 36, 36)",
+    footer:"si ya ingresaste tu nombre selecciona Adelante"
     
 });
 
